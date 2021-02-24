e582e23da1a6bd9d6d84fdc5548d2976d8e3b9f9
Brandon Johnson <brandon.johnson@revature.net>
2021-02-24 16:53:00 -0600

    commented out broken stuff

--- src/main/java/com/revature/util/JwtParser.java
+++ src/main/java/com/revature/util/JwtParser.java
@@ -1,6 +1,5 @@
 package com.revature.util;
 
-import com.revature.restaurant.pojo.Principal;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
@@ -11,32 +10,32 @@ public class JwtParser {
 
     public static void checkToken(HttpServletRequest req) {
 
-
-        // Get the HTTP cookie named Authorization
-        String token = "";
-        Cookie[] cookies = req.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("my-token")) {
-                token = cookie.getValue();
-            }
-        }
-        try {
-
-            Claims claims = Jwts.parser()
-                                .setSigningKey("super-secret-ky")
-                                .parseClaimsJws(token)
-                                .getBody();
-
-            Principal principal = new Principal();
-            principal.setId(claims.getId());
-            principal.setUsername(claims.getSubject());
-            principal.setRole(claims.get("role", String.class));
-
-            req.setAttribute("principal", principal);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//
+//        // Get the HTTP cookie named Authorization
+//        String token = "";
+//        Cookie[] cookies = req.getCookies();
+//        for (Cookie cookie : cookies) {
+//            if (cookie.getName().equals("my-token")) {
+//                token = cookie.getValue();
+//            }
+//        }
+//        try {
+//
+//            Claims claims = Jwts.parser()
+//                                .setSigningKey("super-secret-ky")
+//                                .parseClaimsJws(token)
+//                                .getBody();
+//
+//            Principal principal = new Principal();
+//            principal.setId(claims.getId());
+//            principal.setUsername(claims.getSubject());
+//            principal.setRole(claims.get("role", String.class));
+//
+//            req.setAttribute("principal", principal);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
 
     }
