c56ae9a2368876a3007bae10e9e6304bbf02676f
Brandon Johnson <brandon.johnson@revature.net>
2021-02-24 09:14:22 -0600

    hibernate workingish

--- src/main/java/com/revature/servlets/userServlet.java
+++ src/main/java/com/revature/servlets/userServlet.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 
 public class userServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
+        System.out.println("sdasda");
         Session s = HibernateUtil.getSessionFactory().openSession();
         User newUsers = new User(10,"calvin123","calvin123","calvin","zheng","calvin123@yahoo.com",1);
         s.save(newUsers);
